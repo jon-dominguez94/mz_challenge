4c2d6a57de68232004d34a5e685da4b2c81dd6ee
jondominguez94 <jon.dominguez94@yahoo.com>
2019-02-06 09:44:45 -0800

    Add sections to timer for styling

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -10,17 +10,6 @@ function loadJSON(callback) {
   xobj.send(null);
 }
 
-
-// function init() {
-//   loadJSON(function (response) {
-//     const contents = JSON.parse(response);
-//     // console.log(contents);
-//     return contents;
-//   });
-// }
-
-// const contents = init();
-
 function setTimer(ending){
   const endDate = new Date(ending).getTime();
   let countDown = setInterval(function () {
@@ -33,12 +22,12 @@ function setTimer(ending){
     const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
-    document.getElementById("demo").innerHTML = "Sale ends in " + days + "d " + hours + "h "
+    document.getElementById("countdown").innerHTML = "Sale ends in " + days + "d " + hours + "h "
       + minutes + "m " + seconds + "s!";
 
     if (difference < 0) {
       clearInterval(countDown);
-      document.getElementById("demo").innerHTML = "Sale has ended!";
+      document.getElementById("countdown").innerHTML = "Sale has ended!";
     }
 
   }, 1000);
@@ -49,9 +38,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const contents = JSON.parse(response);
     console.log(contents);
 
-    // document.getElementById("one").style.background = `url(${contents.headerImg})`;
     document.getElementById("header-img").src = contents.headerImg;
-
     setTimer(contents.endDate);
 
   });
