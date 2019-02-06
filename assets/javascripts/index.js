9d490515563e503fc62126489b6ca9e92d618123
jondominguez94 <jon.dominguez94@yahoo.com>
2019-02-06 08:33:30 -0800

    Get json file to read properly

---
+++ assets/javascripts/index.js
@@ -0,0 +1,31 @@
+function loadJSON(callback) {
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open('GET', './assets/javascripts/contents.json', true);
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+      callback(xobj.responseText);
+    }
+  };
+  xobj.send(null);
+}
+
+
+// function init() {
+//   loadJSON(function (response) {
+//     const contents = JSON.parse(response);
+//     // console.log(contents);
+//     return contents;
+//   });
+// }
+
+// const contents = init();
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadJSON(function(response) {
+    const contents = JSON.parse(response);
+    console.log(contents);
+
+
+  });
+});
